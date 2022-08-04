283337ce10cc86e91cd888740b4e0fe4cc4ed523
Mohsin-Hossain <mohsinshawon18@gmail.com>
2022-08-04 13:37:54 +0600

    form done

---
+++ src/firebase-init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBHmzpb0vn-vKAAYFju6QK9MioMyR2jmVM",
+  authDomain: "email-password-auth-2-6fb8c.firebaseapp.com",
+  projectId: "email-password-auth-2-6fb8c",
+  storageBucket: "email-password-auth-2-6fb8c.appspot.com",
+  messagingSenderId: "410847523502",
+  appId: "1:410847523502:web:54f2de74ddc37e3416b33f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
