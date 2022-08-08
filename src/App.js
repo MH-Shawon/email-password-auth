b73e0cc37e4fc3a20515a808b536afd97594ac0e
Mohsin-Hossain <mohsinshawon18@gmail.com>
2022-08-08 10:52:19 +0600

    ljdf

--- src/App.js
+++ src/App.js
@@ -1,45 +1,76 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { getAuth } from 'firebase/auth'
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth'
 import app from './firebase-init';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
 
 
 const auth = getAuth(app)
 
-const handleEmailBlur = (event) => {
-  console.log(event.target.value)
-}
-const handlePasswordBlur = (event) => {
-  console.log(event.target.value)
-}
 
-const handleFormSubmit = (event) => {
-  console.log('form-submitted')
-  event.preventDefault()
-}
 function App() {
+
+  const [email, setEmail] = useState('');
+  const [password, setPasswod] = useState('');
+  const [validated, setValidated] = useState(false);
+  const handleEmailBlur = (event) => {
+    setEmail(event.target.value)
+  }
+  const handlePasswordBlur = (event) => {
+    setPasswod(event.target.value)
+  }
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+     event.stopPropagation();
+      return;
+    }
+    
+    if(!/(?=.*[a-zA-Z >>!#$%&? "<<])[a-zA-Z0-9 >>!#$%&?<< ]/.test(password)){
+      return;
+    }
+    setValidated(true);
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(userCredential => {
+        const user = userCredential.user;
+        console.log(user);
+      })
+      .catch(error => {
+        console.error(error)
+      })
+    event.preventDefault()
+  }
   return (
     <div>
       <div className="registration w-50 mx-auto mt-5">
         <h2 className='text-primary'>
           Please Register!!
         </h2>
-        <Form onSubmit={handleFormSubmit}>
+        <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" />
+            <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" required />
             <Form.Text className="text-muted">
               We'll never share your email with anyone else.
             </Form.Text>
+            <Form.Control.Feedback type="invalid">
+              Please provide a valid email.
+            </Form.Control.Feedback>
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" />
+            <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" required />
+            <Form.Control.Feedback type="invalid">
+              Please provide a valid password.
+            </Form.Control.Feedback>
           </Form.Group>
-          
+
           <Button variant="primary" type="submit">
             Submit
           </Button>
